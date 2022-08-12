d0526f16478f213fed30c55ba447c5be5d389411
Juan Garcia <73263498+juanmgarcia21@users.noreply.github.com>
2022-08-12 19:04:54 -0300

    Delete trabajo-python directory

--- trabajo-python/historial3.py
+++
@@ -1,5 +0,0 @@
-historial3 = (9530, 4120, 4580, 1500, 890, 7516, 426)
-promedio = sum(historial3)/len(historial3)
-print(promedio)
-if promedio > 4500:
-    print("Se ha excedido del gasto promedio para su mascota")
