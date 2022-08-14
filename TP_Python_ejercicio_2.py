a69517295a98112d4bbb8f8e7ef08fc86b831b02
Magali Garcia <60717025+magaligarcia@users.noreply.github.com>
2022-08-14 18:35:10 -0300

    Delete TP_Python_ejercicio_2.py
    Borrando archivos mal ubicados

--- TP_Python_ejercicio_2.py
+++
@@ -1,9 +0,0 @@
-print("Ejercicio 2 - Magali Garcia:")
-dueno2 = [23546987,  "Maria",  "Perez", 4789689,  "Pueyrredon  811"]
-indice = 0
-print("Datos de dueño a excepcion de DNI y Apellido:")
-for i in dueno2:
-    if indice != 0 and indice != 2:
-            print(dueno2[indice])
-    indice+=1
-
